aa326faffbde1ae1f32c765725cf8f359296a032
Stanimir Dimitrov <evlution63@gmail.com>
2023-11-11 10:51:11 +0200

    feat: add a dummy profile pic

--- src/app/comments/components/Comment.tsx
+++ src/app/comments/components/Comment.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Image from 'next/image';
+import img from '../../images/icon-p.png';
 import { IComment } from '../models/Comment';
 
 interface Props {
@@ -8,9 +10,10 @@ interface Props {
 
 export const Comment: React.FunctionComponent<Props> = ({ comment }) => {
   return (
-    <div className='mb-4'>
-      {/* <Image /> */}
-      <div className='flex flex-col'>
+    <div className='mb-4 flex'>
+      <Image src={img} alt='profile-pic' width={80} className='rounded-full' />
+
+      <div className='flex flex-col ml-2'>
         <div className='flex'>
           <span className='font-semibold mr-2'>{`@${comment.username}`}</span>
           <span>12 hours ago</span>
