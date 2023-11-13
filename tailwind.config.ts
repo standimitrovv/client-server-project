61a9796880e77f781a8eb3295e17c6be06e737fd
Stanimir Dimitrov <evlution63@gmail.com>
2023-11-13 10:01:17 +0200

    feat: hamburger menu

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,22 @@ const config: Config = {
       screens: {
         xs: '350px',
       },
+      keyframes: {
+        'open-menu': {
+          '0%': {
+            transform: 'scaleY(0)',
+          },
+          '80%': {
+            transform: 'scaleY(1.2)',
+          },
+          '100%': {
+            transform: 'scaleY(1)',
+          },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      },
     },
   },
   plugins: [],
