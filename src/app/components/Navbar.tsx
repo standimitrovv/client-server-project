7d8a678f63f80791e52f38a21dfde148a1fe4cfc
Stanimir Dimitrov <evlution63@gmail.com>
2023-11-14 09:29:33 +0200

    refactor: clean up

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,9 @@
 'use client';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { PAGE_WIDTH } from '../CommonStyles';
+import { HamburgerButton } from './HamburgerButton';
+import { MobileMenu } from './MobileMenu';
 
 const links = [
   {
@@ -21,6 +23,21 @@ const links = [
 export const Navbar = () => {
   const [isMenuShow, setIsMenuShown] = useState<boolean>(false);
 
+  const pages = useCallback(
+    (additionalStyles?: string, onClick?: () => void) =>
+      links.map((l, index) => (
+        <Link
+          key={`${l.href}${l.text}${index}`}
+          href={l.href}
+          className={`${additionalStyles} hover:opacity-90`}
+          onClick={onClick}
+        >
+          {l.text}
+        </Link>
+      )),
+    []
+  );
+
   return (
     <header className='relative'>
       <section
@@ -29,48 +46,20 @@ export const Navbar = () => {
         <Link href='/'>Stanimir Dimitrov</Link>
 
         <div>
-          <button
-            id='hamburger-button'
-            className={`text-3xl md:hidden cursor-pointer relative w-8 h-8  ${
-              isMenuShow ? 'toggle-btn' : ''
-            }`}
+          <HamburgerButton
+            triggerAnimation={isMenuShow}
             onClick={() => setIsMenuShown(!isMenuShow)}
-          >
-            <div className='bg-white w-8 h-1 rounded absolute top-4 -mt-0.5 transition-all duration-500 before:countent-[""] before:bg-white before:w-8 before:h-1 before:rounded before:absolute before:-translate-x-4 before:-translate-y-3 before:transition-all before:duration-500 after:countent-[""] after:bg-white after:w-8 after:h-1 after:rounded after:absolute after:transition-all after:duration-500 after:-translate-x-4 after:translate-y-3' />
-          </button>
+          />
 
-          <nav className='hidden md:block space-x-8 text-xl'>
-            {links.map((l, index) => (
-              <Link
-                key={`${l.href}${l.text}${index}`}
-                href={l.href}
-                className='hover:opacity-90'
-              >
-                {l.text}
-              </Link>
-            ))}
-          </nav>
+          <nav className='hidden md:block space-x-8 text-xl'>{pages()}</nav>
         </div>
       </section>
-      <section
-        id='mobile-menu'
-        className={`absolute top-68 bg-black w-full text-5xl ${
-          isMenuShow ? 'flex' : 'hidden'
-        } flex-col justify-center origin-top animate-open-menu section-min-height overflow-hidden`}
-      >
-        <nav className='flex flex-col items-center py-8' area-label='mobile'>
-          {links.map((l, index) => (
-            <Link
-              key={`${l.href}${l.text}${index}`}
-              href={l.href}
-              className='w-full text-center py-6 hover:opacity-90'
-              onClick={() => setIsMenuShown(false)}
-            >
-              {l.text}
-            </Link>
-          ))}
-        </nav>
-      </section>
+
+      {isMenuShow && (
+        <MobileMenu>
+          {pages('w-full text-center py-6', () => setIsMenuShown(false))}
+        </MobileMenu>
+      )}
     </header>
   );
 };
