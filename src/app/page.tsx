c1daa05efe2f2c731c65470558e37597706d31a1
Stanimir Dimitrov <evlution63@gmail.com>
2023-11-15 13:25:35 +0200

    feat: make the home page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,16 @@ import picOfMe from './images/me.jpg';
 
 export default function Home() {
   return (
-    <section id='Home page' className='flex w-full mb-8'>
-      <div className='w-[80%]'>
+    <section
+      id='Home page'
+      className='flex flex-col md:flex-row md:justify-between w-full mb-8'
+    >
+      <div className='w-full md:w-[80%]'>
         <Image src={picOfMe} alt='profile picture' className='rounded-md' />
       </div>
 
-      <div className='ml-20 flex flex-col sm:justify-end sm:items-end w-full mb-4'>
-        <div className='mb-10 text-end'>
+      <div className='flex flex-col items-start mt-8 md:ml-20 md:justify-end md:items-end w-full mb-4'>
+        <div className='mb-10 md:text-end'>
           <h1 className='text-4xl'>Hey there 👋,</h1>
           <h2 className='text-3xl'>I am Stan.</h2>
           <h1 className='text-4xl font-extrabold'>Self taught</h1>
