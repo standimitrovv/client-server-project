2c43ff9778634cad8234aa469a7182109b023a67
Stanimir Dimitrov <evlution63@gmail.com>
2023-11-17 17:28:58 +0200

    chore: install react-toastify

--- src/app/contact/components/ContactForm.tsx
+++ src/app/contact/components/ContactForm.tsx
@@ -38,60 +38,62 @@ export const ContactForm = () => {
   };
 
   return (
-    <form onSubmit={(e) => e.preventDefault()} className='flex flex-col'>
-      <label htmlFor='name' className='mb-2'>
-        Full Name
-      </label>
-      <input
-        id='name'
-        name='name'
-        type='text'
-        required
-        placeholder='What is your name?'
-        className='mb-6 rounded-md text-black p-2'
-        value={fullName}
-        onChange={(e) => setFullName(e.target.value)}
-      />
+    <>
+      <form onSubmit={(e) => e.preventDefault()} className='flex flex-col'>
+        <label htmlFor='name' className='mb-2'>
+          Full Name
+        </label>
+        <input
+          id='name'
+          name='name'
+          type='text'
+          required
+          placeholder='What is your name?'
+          className='mb-6 rounded-md text-black p-2'
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+        />
 
-      <label htmlFor='email' className='mb-2'>
-        E-mail
-      </label>
-      <input
-        id='email'
-        name='email'
-        type='email'
-        required
-        placeholder='What is your email?'
-        className='mb-6 rounded-md text-black p-2'
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+        <label htmlFor='email' className='mb-2'>
+          E-mail
+        </label>
+        <input
+          id='email'
+          name='email'
+          type='email'
+          required
+          placeholder='What is your email?'
+          className='mb-6 rounded-md text-black p-2'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-      <label htmlFor='message' className='mb-2'>
-        Message
-      </label>
-      <textarea
-        required
-        name='message'
-        id='message'
-        placeholder='What do you want to tell/ask me?'
-        rows={4}
-        cols={20}
-        className='rounded-md mb-6 text-black p-2'
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
+        <label htmlFor='message' className='mb-2'>
+          Message
+        </label>
+        <textarea
+          required
+          name='message'
+          id='message'
+          placeholder='What do you want to tell/ask me?'
+          rows={4}
+          cols={20}
+          className='rounded-md mb-6 text-black p-2'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
 
-      <button
-        type='submit'
-        disabled={isSubmitButtonDisabled}
-        onClick={handleFormSubmit}
-        className={`border rounded-md py-2 ${
-          isSubmitButtonDisabled ? 'bg-gray-400 text-black' : 'bg-green-400'
-        }`}
-      >
-        Send
-      </button>
-    </form>
+        <button
+          type='submit'
+          disabled={isSubmitButtonDisabled}
+          onClick={handleFormSubmit}
+          className={`border rounded-md py-2 ${
+            isSubmitButtonDisabled ? 'bg-gray-400 text-black' : 'bg-green-400'
+          }`}
+        >
+          Send
+        </button>
+      </form>
+    </>
   );
 };
