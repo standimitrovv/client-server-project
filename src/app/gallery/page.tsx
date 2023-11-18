e738308c570852d590b59c5f4d21b99a949232d7
Stanimir Dimitrov <evlution63@gmail.com>
2023-11-18 12:21:48 +0200

    chore: add 2 more images

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -6,8 +6,10 @@ import img4 from '../images/gallery/img4.jpg';
 import img5 from '../images/gallery/img5.jpg';
 import img6 from '../images/gallery/img6.jpg';
 import img7 from '../images/gallery/img7.jpg';
+import img8 from '../images/gallery/img8.jpg';
+import img9 from '../images/gallery/img9.jpg';
 
-const images = [img1, img2, img3, img4, img5, img6, img7];
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9];
 
 export default function Gallery() {
   return (
