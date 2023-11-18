acd3f30110241a0c89849e13f3b3abf49d7a7565
Stanimir Dimitrov <evlution63@gmail.com>
2023-11-18 12:20:05 +0200

    feat: create the gallery of images

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,3 +1,27 @@
+import Image from 'next/image';
+import img1 from '../images/gallery/img1.jpg';
+import img2 from '../images/gallery/img2.jpg';
+import img3 from '../images/gallery/img3.jpg';
+import img4 from '../images/gallery/img4.jpg';
+import img5 from '../images/gallery/img5.jpg';
+import img6 from '../images/gallery/img6.jpg';
+import img7 from '../images/gallery/img7.jpg';
+
+const images = [img1, img2, img3, img4, img5, img6, img7];
+
 export default function Gallery() {
-  return <section>Gallery page</section>;
+  return (
+    <section id='gallery'>
+      <div className='grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        {images.map((i, index) => (
+          <Image
+            key={index}
+            src={i}
+            alt={`Image №${index}`}
+            className='sm:w-80 sm:h-80 sm:justify-self-center'
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
