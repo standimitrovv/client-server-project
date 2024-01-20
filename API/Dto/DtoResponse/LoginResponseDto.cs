9d4204cd26bdf3b9069855eb2afa226bdb4c43ed
Stanimir Dimitrov <evlution63@gmail.com>
2024-01-20 11:20:41 +0200

    feat: add a LoginResponseDto

---
+++ API/Dto/DtoResponse/LoginResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Dto.DtoResponse
+{
+    public class LoginResponseDto
+    {
+        public UserDto User { get; set; }
+
+        public string Token { get; set; }
+    }
+}
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eae287d2adb48dfce7c8b461c53ab006eb3ed53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8e66daae370bc68b10eb334d040b79c014a0096")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
