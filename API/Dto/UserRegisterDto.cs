f8e66daae370bc68b10eb334d040b79c014a0096
Stanimir Dimitrov <evlution63@gmail.com>
2024-01-20 11:18:25 +0200

    feat: create a User model and DTOs for login and register

---
+++ API/Dto/UserRegisterDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dto
+{
+    public class UserRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
