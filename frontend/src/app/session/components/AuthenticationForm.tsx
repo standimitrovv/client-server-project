bf835763c3adce9ec6062a70cd36ef322b092fbc
Stanimir Dimitrov <evlution63@gmail.com>
2024-01-22 12:24:43 +0200

    style:change the paddding on sm screen

--- frontend/src/app/session/components/AuthenticationForm.tsx
+++ frontend/src/app/session/components/AuthenticationForm.tsx
@@ -7,7 +7,7 @@ export const AuthenticationForm = () => {
 
   return (
     <div className='max-w-xl m-auto h-screen flex flex-col justify-center items-center'>
-      <div className='h-fit p-16 border rounded-md w-full'>
+      <div className='h-fit p-10 md:p-16 border rounded-md w-full'>
         {isSigningIn ? <Login /> : <Register />}
       </div>
     </div>
